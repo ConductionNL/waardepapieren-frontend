37bcd39d42f4ff869b9c558b9daf974bbd886742
ginokok1996 <ginokok1996@hotmail.com>
2021-11-29 16:19:55 +0100

    home page

--- pwa/src/components/common/footer.tsx
+++ pwa/src/components/common/footer.tsx
@@ -1,5 +1,4 @@
 import * as React from "react";
-import { ThemeSwitcher } from "../theme-switcher/theme-switcher";
 
 export default function Footer() {
   return (
@@ -16,35 +15,30 @@ export default function Footer() {
                 href="tel:14024"
                 className="utrecht-link utrecht-link--telephone"
               >
-                14024
+                0229 25 22 00
               </a>
             </div>
             <div>
               <i className="fas fa-at mr-2" />
               <a
-                href="mailto:gemeente@nijmegen.nl"
+                href="mailto:gemeente@hoorn.nl"
                 className="utrecht-link utrecht-link--telephone"
               >
-                ifo@demodam.nl
+                gemeente@hoorn.nl
               </a>
             </div>
           </div>
           <div className="col-12 col-sm-6">
             <div className="float-right">
-              <h3 className="utrecht-heading-3 utrecht-heading-3--distanced">
-                Openingstijden
+              <h3 className="utrecht-heading-3 utrecht-heading-3--distanced mb-2">
+                Openingstijden:
               </h3>
               <p className="utrecht-paragraph utrecht-paragraph--distanced">
-                <strong>Maandag - woensdag: 9.00 - 17.00</strong>
-                <br />
-                <strong>Donderdag: 9.00 - 20.00</strong>
-                <br />
-                <strong>Vrijdag: 9.00 - 17.00</strong>
+                <p className={"mb-0 mt-0"}>Maandag - Vrijdag: 8.00 - 17.00</p>
+                <p className={"mb-0 mt-0"}>Donderdag: 8.00 - 20.00</p>
+                <p className={"mb-0 mt-0"}>Vrijdag: 8.00 - 17.00</p>
               </p>
             </div>
-          </div>
-          <div className="col-12 ml-auto fit-content">
-            <ThemeSwitcher />
           </div>
         </div>
       </div>
--- pwa/src/pages/index.tsx
+++ pwa/src/pages/index.tsx
@@ -1,33 +1,59 @@
 import * as React from "react";
 import Layout from "../components/common/layout";
 import { useUrlContext } from "../context/urlContext";
+import { isLoggedIn } from "../services/auth";
 
 const IndexPage = () => {
   const context = useUrlContext();
-
-  React.useEffect(() => {
-    console.log(context.apiUrl);
-    console.log(context.meUrl);
-  }, []);
-
   return (
     <Layout>
       <main>
-        <title>Skeleton app</title>
-        <div>
-          <h1 className="utrecht-heading-1 utrecht-heading-1--distanced">
-            Welkom
-          </h1>
-          <h4 className="utrecht-heading-4 utrecht-heading-4--distanced">
-            Dit is de skeleton NL Design applicatie.
-          </h4>
-          <p className="utrecht-p">
-            Het doel van deze skeleton applicatie is om meerdere design tokens
-            te testen over een set NL Design componenten. Ook zou je deze
-            applicatie als template kunnen gebruiken om zelf een app te bouwen
-            in NL Design. De link naar onze github repo:
-            https://github.com/ConductionNL/nl-design-skeleton-gatsby
-          </p>
+        <title>Waardepapieren</title>
+        <div className={"text-center"}>
+          {isLoggedIn() ? (
+            <>
+              <h1 className="utrecht-heading-1 utrecht-heading-1--distanced">
+                Welkom bij Waardepapieren
+              </h1>
+              <h4 className="utrecht-heading-4 utrecht-heading-4--distanced">
+                Met deze applicatie kunt u uw waardepapieren bekijken en
+                aanvragen.
+              </h4>
+              <a
+                className={"utrecht-link mt-4"}
+                href={
+                  context.baseUrl +
+                  "/digid/login?returnUrl=" +
+                  context.frontendUrl +
+                  "/redirect"
+                }
+              >
+                <button className={"utrecht-button"}>Doorgaan</button>
+              </a>
+            </>
+          ) : (
+            <>
+              <h1 className="utrecht-heading-1 utrecht-heading-1--distanced">
+                Welkom bij Waardepapieren
+              </h1>
+              <h4 className="utrecht-heading-4 utrecht-heading-4--distanced">
+                U dient ingelogd te zijn om door te gaan.
+              </h4>
+              <div className={"mt-4"}>
+                <a
+                  className={"utrecht-link"}
+                  href={
+                    context.baseUrl +
+                    "/digid/login?returnUrl=" +
+                    context.frontendUrl +
+                    "/redirect"
+                  }
+                >
+                  <button className={"utrecht-button"}>Inloggen</button>
+                </a>
+              </div>
+            </>
+          )}
         </div>
       </main>
     </Layout>
