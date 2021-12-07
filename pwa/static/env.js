648bdb1dc5736998f2b76d3fabca935a74679d95
ginokok1996 <ginokok1996@hotmail.com>
2021-12-07 14:49:51 +0100

    env .js

---
+++ pwa/static/env.js
@@ -0,0 +1,7 @@
+window.GATSBY_API_URL = "https://acc-waardepapieren.hoorn.nl/api";
+window.GATSBY_ME_URL = "https://acc-waardepapieren.hoorn.nl/api/users/me";
+window.GATSBY_BASE_URL = "https://acc-waardepapieren.hoorn.nl";
+window.GATSBY_FRONTEND_URL = "https://acc-waardepapieren.hoorn.nl";
+window.GATSBY_ORGANIZATION = "placeholder";
+window.GATSBY_REDIRECT_URL = "null";
+window.GATSBY_DEFAULT_THEME = "hoorn";
