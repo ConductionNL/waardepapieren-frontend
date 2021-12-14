166a9cb7c25d295442b9f047195aa276fb66cdc6
ginokok1996 <ginokok1996@users.noreply.github.com>
2021-12-14 15:37:29 +0000

    deployed via Gatsby Publish Action 🎩 for 0164f35fe987f6f1a7879d2491f6f1d5e3c9f7a5

---
+++ env.js
@@ -0,0 +1,7 @@
+window.GATSBY_API_URL = "http://localhost/api";
+window.GATSBY_ME_URL = "http://localhost/me";
+window.GATSBY_BASE_URL = "http://localhost";
+window.GATSBY_FRONTEND_URL = "http://localhost:9000";
+window.GATSBY_ORGANIZATION = "placeholder";
+window.GATSBY_REDIRECT_URL = "null";
+window.GATSBY_DEFAULT_THEME = "hoorn";
